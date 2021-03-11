9c9ee2f9bcd5f2b4bb36ad33eab0ee574aead35c
Jérémy ARGOUD <jargoud@webqam.fr>
2021-03-11 19:43:26 +0100

    Add DropzoneController.php

---
+++ src/Http/Controllers/DropzoneController.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace Jargoud\LaravelBackpackDropzone\Http\Controllers;
+
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\UploadedFile;
+use Illuminate\Routing\Controller;
+use Pion\Laravel\ChunkUpload\Exceptions\UploadMissingFileException;
+use Pion\Laravel\ChunkUpload\Handler\AbstractHandler;
+use Pion\Laravel\ChunkUpload\Receiver\FileReceiver;
+
+class DropzoneController extends Controller
+{
+    /**
+     * Handles the file upload
+     *
+     * @param FileReceiver $receiver
+     * @return JsonResponse
+     * @throws UploadMissingFileException
+     */
+    public function uploadFile(FileReceiver $receiver): JsonResponse
+    {
+        // check if the upload is success, throw exception or return response you need
+        if ($receiver->isUploaded() === false) {
+            throw new UploadMissingFileException();
+        }
+        // receive the file
+        $save = $receiver->receive();
+
+        // check if the upload has finished (in chunk mode it will send smaller files)
+        if ($save->isFinished()) {
+            // save the file and return any response you need
+            return $this->saveFile($save->getFile());
+        }
+
+        // we are in chunk mode, lets send the current progress
+        /** @var AbstractHandler $handler */
+        $handler = $save->handler();
+
+        return response()->json([
+            "done" => $handler->getPercentageDone(),
+        ]);
+    }
+
+    /**
+     * Saves the file
+     *
+     * @param UploadedFile $file
+     * @return JsonResponse
+     */
+    protected function saveFile(UploadedFile $file): JsonResponse
+    {
+        $fileName = $this->createFilename($file);
+        // Group files by mime type
+        $mime = str_replace('/', '-', $file->getMimeType());
+        // Group files by the date (week
+        $dateFolder = date("Y-m-W");
+
+        // Build the file path
+        $filePath = "upload/{$mime}/{$dateFolder}/";
+        $finalPath = storage_path("app/" . $filePath);
+
+        // move the file name
+        $file->move($finalPath, $fileName);
+
+        return response()->json([
+            'path' => $filePath,
+            'name' => $fileName,
+            'mime_type' => $mime,
+        ]);
+    }
+
+    /**
+     * Create unique filename for uploaded file
+     * @param UploadedFile $file
+     * @return string
+     */
+    protected function createFilename(UploadedFile $file): string
+    {
+        $extension = $file->getClientOriginalExtension();
+
+        // Filename without extension
+        $filename = str_replace("." . $extension, "", $file->getClientOriginalName());
+
+        // Add timestamp hash to name of the file
+        $filename .= "_" . md5(time()) . "." . $extension;
+
+        return $filename;
+    }
+}
