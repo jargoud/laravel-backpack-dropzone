d8ac89ededbc3513cfaa1e156e313af4824ea40a
Jérémy ARGOUD <jargoud@webqam.fr>
2023-05-24 14:51:20 +0200

    feat(field): update for Backpack 5

--- src/resources/views/fields/dropzone.blade.php
+++ src/resources/views/fields/dropzone.blade.php
@@ -13,41 +13,41 @@
 @endphp
 
 @include('crud::fields.inc.wrapper_start')
-<label>{!! $field['label'] !!}</label>
-@include('crud::fields.inc.translatable_icon')
+  <label>{!! $field['label'] !!}</label>
+  @include('crud::fields.inc.translatable_icon')
 
-<div
-  id="{{ $id }}"
-  class="dropzone"
-  data-init-function="bpFieldInitDropzoneElement"
-  data-config='@json($field['config'] ?? [], JSON_FORCE_OBJECT)'
-  data-allow-multiple='@json(!!$field['allow_multiple'])'
->
-  <input
-    @if(!empty($field['value'])) data-value='@json($field['value'])' @endif
-    type="hidden"
-    data-name="{{ $field['name'] }}"
-    style="position:absolute; height: 1px; width: 1px; opacity: 0;"
-  />
-  <div data-input-list></div>
-</div>
-
-{{-- HINT --}}
-@if(isset($field['hint']))
-  <p class="help-block">{!! $field['hint'] !!}</p>
-  @endif
+  <div
+    id="{{ $id }}"
+    class="dropzone"
+    data-init-function="bpFieldInitDropzoneElement"
+    data-config='@json($field['config'] ?? [], JSON_FORCE_OBJECT)'
+    data-allow-multiple='@json(!!$field['allow_multiple'])'
+  >
+    <input
+      @if(!empty($field['value'])) data-value='@json($field['value'])' @endif
+      type="hidden"
+      data-name="{{ $field['name'] }}"
+      style="position:absolute; height: 1px; width: 1px; opacity: 0;"
+    />
+    <div data-input-list></div>
   </div>
 
-  @if($crud->fieldTypeNotLoaded($field))
-    @php
-      $crud->markFieldTypeAsLoaded($field);
-    @endphp
+  {{-- HINT --}}
+  @if(isset($field['hint']))
+    <p class="help-block">{!! $field['hint'] !!}</p>
+    @endif
+@include('crud::fields.inc.wrapper_end')
 
-    @push('crud_fields_styles')
-      <link href="{{ asset('packages/dropzone/css/dropzone.css') }}" rel="stylesheet" type="text/css"/>
-    @endpush
+@if($crud->fieldTypeNotLoaded($field))
+  @php
+    $crud->markFieldTypeAsLoaded($field);
+  @endphp
 
-    @push('crud_fields_scripts')
-      <script src="{{ asset('packages/dropzone/js/dropzone.js') }}"></script>
-    @endpush
-  @endif
+  @push('crud_fields_styles')
+    <link href="{{ asset('packages/dropzone/css/dropzone.css') }}" rel="stylesheet" type="text/css"/>
+  @endpush
+
+  @push('crud_fields_scripts')
+    <script src="{{ asset('packages/dropzone/js/dropzone.js') }}"></script>
+  @endpush
+@endif
